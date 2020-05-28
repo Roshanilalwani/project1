c6f4465a4fede73bb1689570444af850f0de4f21
Roshanilalwani <64470205+Roshanilalwani@users.noreply.github.com>
2020-05-28 18:36:20 +0530

    Update rebuild_failed_notifier.py

--- rebuild_failed_notifier.py
+++ rebuild_failed_notifier.py
@@ -1,9 +1,9 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-host_address = "shubhamkhandelwal523@gmail.com"
-host_pass = "549021916shUB"
-guest_address = "shubhamkhandelwal523@gmail.com"
+host_address = "d990fe65cc9e70"
+host_pass = "3df080eb7596ec"
+guest_address = "roshanilalwani1111@gmail.com"
 subject = "Regarding failure of rebuild.py"
 content = '''Hello, 
 				Developer is in the last commit regarding error . it is rebuild agin
@@ -13,7 +13,7 @@ message['From'] = host_address
 message['To'] = guest_address
 message['Subject'] = subject
 message.attach(MIMEText(content, 'plain'))
-session = smtplib.SMTP('smtp.gmail.com', 587)
+session = smtplib.SMTP('smtp.gmail.com', 2525)
 session.starttls()
 session.login(host_address, host_pass)
 text = message.as_string()
