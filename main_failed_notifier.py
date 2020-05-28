522500cbf35fd80e050687351c27cb52eb6dac02
Roshanilalwani <64470205+Roshanilalwani@users.noreply.github.com>
2020-05-28 18:33:20 +0530

    Update main_failed_notifier.py

--- main_failed_notifier.py
+++ main_failed_notifier.py
@@ -1,7 +1,7 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-host_address = "smtp.mailtrap.io"
+host_address = "d990fe65cc9e70"
 host_pass = "3df080eb7596ec"
 guest_address = "roshanilalwani1111@gmail.com"
 subject = "Regarding failure of main.py"
@@ -13,7 +13,7 @@ message['From'] = host_address
 message['To'] = guest_address
 message['Subject'] = subject
 message.attach(MIMEText(content, 'plain'))
-session = smtplib.SMTP('smtp.gmail.com', 587)
+session = smtplib.SMTP('smtp.gmail.com', 2525)
 session.starttls()
 session.login(host_address, host_pass)
 text = message.as_string()
